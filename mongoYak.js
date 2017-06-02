6c49cfd9d339a01650fefa42e41a2b3f28b1ac2a
Rima Alrwais <ralrwais@ralrwais-ltm2.internal.salesforce.com>
2017-06-02 11:48:22 -0700

    initail push

---
+++ mongoYak.js
@@ -0,0 +1,19 @@
+{
+ “_id” : ObjectId(),
+ “name” : “Rima Alrwais”,
+ “handle” : “ralrwais”,
+ “password” : “556624370”,
+ “phone” : “8888888888”,
+ “email” : “alrwaisrima@gmail.com”,
+ “friends” : [
+    {
+      “name” : “abc”,
+      “status” : “Friend”
+    },
+    {
+      “name” : “xyz”,
+      “status” : “Friend”
+    }
+ ],
+ “__v” : 0
+}
